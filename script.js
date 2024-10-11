27c15e118e265ec32955415e1deeb860ba7d2315
JulioTri15 <82088750+JulioTri15@users.noreply.github.com>
2024-10-11 20:03:44 +0800

    Made navbar responsive

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Get the hamburger icon and navigation menu
 const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('nav');
+const navList = document.querySelector('.nav-list');
 
-// Add event listener to toggle the 'active' class when the hamburger is clicked
 hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active');
+    navList.classList.toggle('active'); // Toggle visibility
 });
